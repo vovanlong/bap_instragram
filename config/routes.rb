3e1b20306d89b6931f979d34031247e8d124f288
vovanlong <vovanlong1997@gmail.com>
2018-07-15 01:57:48 +0700

    final user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     path: '',
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
     controllers: {omniauth_callbacks: 'omniauth_callbacks'}
-  
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
