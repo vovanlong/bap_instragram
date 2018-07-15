e898d59d7e5122af2f8387d4b49b567ee30348b7
vovanlong <vovanlong1997@gmail.com>
2018-07-16 01:07:01 +0700

    model Posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
     controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
+  resources :posts, only: [:index, :show, :create] do
+    resources :photos, only: [:create]
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
